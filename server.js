dfbefeaea89ecb7802d25d12e8bc0493be7885dd
fendishatech <liyukibir@gmail.com>
2023-06-21 00:50:11 -0700

    layout

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ const indexRouter = require("./routes/index");
 const usersRouter = require("./routes/users");
 const app = express();
 
+// MIDDLEWARES
+app.use(expressLayouts);
+app.set("view engine", "ejs");
+
 // ROUTES
 app.use("/", indexRouter);
 app.use("/", usersRouter);
