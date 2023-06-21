653bf70269b799026475fd07ff5e0509bcf28275
fendishatech <liyukibir@gmail.com>
2023-06-21 06:19:46 -0700

    validation

--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,41 @@ router.get("/register", (req, res) => {
   res.render("register");
 });
 
+router.post("/login", (req, res) => {
+  console.log(req.body);
+  res.render("login");
+});
+
+router.post("/register", (req, res) => {
+  const { name, email, password, password2 } = req.body;
+
+  let errors = [];
+
+  // validations
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "all feilds are required and can not be empty!" });
+  }
+
+  if (password != password2) {
+    errors.push({ msg: "Passwords do not match!" });
+  }
+
+  // check password length
+  if (password.length < 3) {
+    errors.push({ msg: "Password ,ust be at least 3 characters long!" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2,
+    });
+  } else {
+    res.render("dashboard", { user: { name: "Kidus" } });
+  }
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.static("public"));
 app.set("views", path.join(__dirname, "./views"));
 app.set("view engine", "ejs");
 app.set("layout", "layout");
+app.use(express.urlencoded({ extended: false }));
 
 // ROUTES
 app.use("/", indexRouter);
